c301503532645ce89d3a2629e781984b1de0bc99
cristian sanchez <cristiansanchez2715@gmail.com>
2023-06-05 10:13:52 -0500

    actualizando aplicacion

--- generator/Vainilla/Generator.js
+++ generator/Vainilla/Generator.js
@@ -1,3 +1,4 @@
+//FACILES
 var A = "Desafío: Calcular la suma de dos números.<br> Escribe una función llamada sumar que tome dos parámetros numéricos,<br> a y b, y devuelva la suma de ambos."
 var B = "Desafío: Saludo básico. <br> Escribe una función llamada saludar que no tome ningún parámetro<br> y simplemente imprima en la consola el mensaje  Hola, mundo!."
 var C = "Desafío: Obtener el doble de un número. <br> Escribe una función llamada obtenerDoble que tome un número como parámetro<br> y devuelva el doble de ese número."
@@ -8,28 +9,43 @@ var G = "Desafío: multiplicar dos números. <br>Escribe una línea de código q
 var H = "Desafío: Verificar si un número es positivo, negativo o cero. <br>Escribe una función llamada verificarNumero que tome un número como parámetro<br> y muestre en la consola si el número es positivo, negativo o cero."
 var I = "Desafío: Imprimir los números del 1 al 5.<br> Escribe un bucle for que imprima los números del 1 al 5 en la consola."
 var J = "Desafío: Verificar si un número es positivo.<br> Escribe una función llamada verificarPositivo que tome un número como parámetro<br> y muestre en la consola si el número es positivo o no."
+//MEDIO
 var K = "Desafío: Calcular la suma de los números pares del 1 al 10. <br>Escribe un código que calcule la suma de todos los números pares del 1 al 10 y<br> muestre el resultado en la consola."
 var L = "Desafío: Verificar si un número es par o impar. <br>Escribe una función llamada verificarParImpar que tome un número como parámetro<br> y muestre en la consola si el número es par o impar."
 var M = "Desafío: Filtrar números pares de una matriz.<br> Escribe una función llamada filtrarNumerosPares que tome una matriz de números como parámetro<br> y devuelva una nueva matriz que contenga solo los números pares."
 var N = "Desafio: Encontrar numeros primos.<br>Escribe una función llamada encontrarNumerosPrimos que tome dos números enteros (inicio y fin) como parámetros<br> y devuelva un arreglo con todos los números primos encontrados<br> en ese rango, incluyendo los límites."
-var Ñ = ""
+var Ñ = "Desafio: Invertir una cadena. Escribe una funcion llamada invertirCadena que reciba como parametro una cadena de strings y retorne ese mismo string escrito al revez."
 var O = "Desafío: Encontrar la suma de los elementos únicos de un arreglo. <br>Escribe una función llamada sumaElementosUnicos que tome un arreglo de números<br> como parámetro y devuelva la suma de los elementos únicos."
 var P = "Desafío: Filtrar números mayores que un valor dado. <br>Escribe una función llamada filtrarMayores que tome un arreglo de números y<br> un valor mínimo como parámetros, y devuelva un nuevo arreglo con<br> los números mayores que el valor dado."
 var Q = "Escribe una función llamada calcularSuma que tome un arreglo bidimensional.<br> ejemplo: numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; de números como parámetro<br> y calcule la suma de todos los elementos. "
 var R = "Desafío: Obtener una lista de nombres completos a partir de una base de datos de personas.<br> Supongamos que tienes una base de datos de personas representada <br>como un arreglo de objetos. Cada objeto tiene las<br> propiedades nombre y apellido. Tu objetivo es obtener un nuevo arreglo que<br> contenga los nombres completos de todas las personas en la base de datos."
 var S = "Desafío: Convertir un arreglo de números en<br> un nuevo arreglo con sus cuadrados.<br> Escribe una función llamada calcularCuadrados que<br> tome un arreglo de números como parámetro y devuelva<br> un nuevo arreglo con el cuadrado de cada número"
+//DIFICIL
 var T = "Desafio: Encontrar factorial. <br> Escribe una función llamada calcularFactorial que tome un número entero como parámetro y <br>calcule su factorial. El factorial de un número se calcula multiplicando todos<br> los números enteros positivos desde 1 hasta el número dado."
 var U = "Desafío: Calcular el número de Fibonacci para un índice dado. <br>Escribe una función llamada fibonacci que tome<br> un número n como parámetro y devuelva el número<br> de Fibonacci correspondiente al índice n."
 var V = "Desafio: Supongamos que tienes una base de datos de empleados representada como un arreglo de objetos.<br> Cada objeto contiene las propiedades nombre, edad y salario.<br> Tu objetivo es filtrar los empleados que tienen más de 30 años,<br> ordenarlos por salario de mayor a menor y devolver un nuevo arreglo con los nombres de esos empleados. "
 var W = "Desafío: Implementar el algoritmo de ordenamiento quicksort de manera recursiva.<br> Tu tarea es escribir una función llamada quicksort que tome un arreglo de números como parámetro y lo ordene utilizando el algoritmo quicksort de manera recursiva. Aquí tienes una descripción del algoritmo:"
-var X = "Desafío: Dado un arreglo de números, encontrar el par de elementos<br> contiguos cuya suma sea la mayor posible y devolver esa suma.<br><br> Tu tarea es escribir una función llamada encontrarMayorSuma<br> que tome un arreglo de números como parámetro y utilice<br> el método forEach para encontrar el par de elementos contiguos<br> con la suma máxima. La función debe devolver la suma de esos<br> dos elementos"
+var X = "Desafío: Dado un arreglo de números, encontrar el par de elementos<br> contiguos cuya suma sea la mayor posible y devolver esa suma.<br><br> Tu tarea es escribir una función llamada encontrarMayorSuma que tome un arreglo de números como parámetro y encuentre el par de elementos contiguos con la suma máxima."
 var Y = "Desafio: Dado un arreglo de números, encontrar el producto<br><br> de los elementos pares y devolver ese valor. Tu tarea<br> es escribir una función llamada calcularProductoPares<br> que tome un arreglo de números como parámetro y utilice el método <br>reduce para calcular el producto de los elementos pares del arreglo. La función<br> debe devolver el valor del producto"
-var Z = "Desafío: Dado un arreglo de números, encuentra la subsecuencia<br> de elementos consecutivos con la suma más grande y devuelve esa suma.<br><br> Tu tarea es escribir una función llamada encontrarSumaMaxima que tome un arreglo<br> de números como parámetro y encuentre la subsecuencia de elementos<br> consecutivos con la suma más grande. La función debe devolver<br> la suma máxima encontrada."
+var Z = "Desafío: Dado un arreglo de números, encuentra la subsecuencia<br> de elementos consecutivos con la suma más grande y devuelve esa suma.<br><br> Tu tarea es escribir una función llamada encontrarSumaMaxima que tome un arreglo de números como parámetro y encuentre la subsecuencia."
 var AA = "Desafío: Dado un arreglo de objetos que representan productos en un carrito de compras,<br> encuentra la combinación de productos que tenga el menor costo total pero cumpla con un presupuesto máximo dado.<br><br>"
 var BB = "Desafío: Dado un arreglo de números enteros, encuentra todos los pares de elementos cuya suma sea igual a un valor objetivo dado. Tu tarea es escribir una función llamada encontrarParesSumaObjetivo que tome como parámetros un arreglo de números enteros y un valor objetivo, y devuelva un arreglo con todos los pares de elementos cuya suma sea igual al valor objetivo."
-var CC = "Desafío: Dado un número entero, verifica si es un número de Armstrong.<br>Un número de Armstrong es aquel que es igual a la suma de sus dígitos<br> elevados a la potencia del número total de dígitos.<br><br>Tu tarea es escribir una función llamada esNumeroArmstrong que tome<br> como parámetro un número entero y devuelva true si es un número de Armstrong,<br> o false en caso contrario."
-
-
+var CC = "Desafío: Dado un número entero, verifica si es un número de Armstrong.<br>Un número de Armstrong es aquel que es igual a la suma de sus dígitos<br> elevados a la potencia del número total de dígitos.<br><br>Tu tarea es escribir una función llamada esNumeroArmstrong que tome<br> como parámetro un número entero y devuelva true si es un número de Armstrong, o false en caso contrario."
+var DD = "Desafio: "
+var EE = "Desafio: "
+var FF = "Desafio: "
+var GG = "Desafio: "
+var HH = "Desafio: "
+var II = "Desafio: "
+var JJ = "Desafio: "
+var KK = "Desafio: "
+var LL = "Desafio: "
+var MM = "Desafio: "
+var NN = "Desafio: "
+var OO = "Desafio: "
+var PP = "Desafio: "
+var QQ = "Desafio: "
+var RR = "Desafio: "
 
 
 
@@ -119,17 +135,27 @@ parrafo.innerHTML = definitivo
 }
 
 var base1 = "[<br>  { nombre: 'John', edad: 35, salario: 5000 },<br>  { nombre: 'Jane', edad: 28, salario: 4000 },<br> { nombre: 'Mike', edad: 42, salario: 6000 }, <br> { nombre: 'Emily', edad: 31, salario: 5500 }<br> ]"
-var base2 = "[<br>  {marca: Toyota, modelo: Corolla, año: 2020, color: Blanco, precio: 25000,},<br>{marca: Honda,modelo: Civic,año: 2018,color: Gris,precio: 22000, }, <br> {marca: Ford,modelo: Mustang,año: 2021,color: Rojo,precio: 45000,}<br>]"
-var base3 = "[<br>  { marca: Chevrolet, modelo: Camaro, año: 2019, color: Negro, precio: 40000, },<br> { marca: Nissan, modelo: Sentra, año: 2022, color: Plata, precio: 24000, }, <br> { marca: BMW, modelo: X5, año: 2020, color: Azul, precio: 55000,},<br>]"
+var base2 = "[<br> {nombre: Hamburguesa, precio: 5.99},<br> {nombre: Pizza, precio: 8.99}, <br> {nombre: Papas fritas, precio: 2.99},<br> {nombre: Hot dog, precio: 3.99},<br> {nombre: Pollo frito, precio: 6.99},<br>]"
+var base3 = "[<br> { nombre: Mangostán, precio: 5 },<br> { nombre: Rambután, precio: 3}, <br>{ nombre: Pitahaya (fruta del dragón), precio: 4 }, <br>{ nombre: Kiwano (pepino africano),precio: 2 },<br>]"
 var base4 = "[<br>  { nombre: 'Camisa', precio: 20 },<br> { nombre: 'Pantalón', precio: 40 },<br> { nombre: 'Zapatos', precio: 50 },<br> { nombre: 'Cinturón', precio: 10 },<br>]"
-var base5 = "[<br>  { nombre: Vodka, precio: 20.99 },<br> { nombre: Ron, precio: 18.50 },<br> { nombre: Whisky, precio: 35.75 },<br> { nombre: Tequila, precio: 24.90 },  <br>{ nombre: Gin, precio: 22.50 },<br> { nombre: Cerveza, precio: 5.99 },<br> { nombre: Vino tinto, precio: 12.75 },<br> { nombre: Champán, precio: 45.50 },<br> { nombre: Brandy, precio: 28.25 },<br> { nombre: Licor de café, precio: 16.80 } <br>]"
+var base5 = "[<br>  { nombre: Vodka, precio: 20.99 },<br> { nombre: Ron, precio: 18.50 },<br> { nombre: Whisky, precio: 35.75 },<br> { nombre: Tequila, precio: 24.90 },  <br>{ nombre: Gin, precio: 22.50 },<br>]"
 var base6 = "[<br>  { nombre: Fender Stratocaster, precio: 1500 },<br> { nombre: Gibson Les Paul, precio: 2500 }, <br>{ nombre: Ibanez RG, precio: 1000 },<br> { nombre: PRS Custom 24, precio: 3000 }, <br> { nombre: Fender Telecaster, precio: 1200 },<br> { nombre: Gretsch White Falcon, precio: 3500 },<br> { nombre: Epiphone Casino, precio: 800 },<br> { nombre: Jackson Soloist, precio: 1800 },<br> { nombre: Schecter Hellraiser, precio: 1400 },<br> { nombre: G&L Legacy, precio: 1100 }<br>]"
-var base7 = "[<br>  {nombre: Metallica, fechaInicio: 1981-10-28 },<br> { nombre: Iron Maiden, fechaInicio: 1975-12-25 },<br> { nombre: Black Sabbath, fechaInicio: 1968-02-13 }, <br> { nombre: Slayer, fechaInicio: 1981-01-01 },<br> { nombre: Megadeth, fechaInicio: 1983-06-12 },<br> { nombre: Judas Priest, fechaInicio: 1969-02-01 },<br> { nombre: Pantera, fechaInicio: 1981-08-27 },<br> { nombre: Opeth, fechaInicio: 1989-03-01 },<br> { nombre: Sepultura, fechaInicio: 1984-04-04 },<br> { nombre: Anthrax, fechaInicio: 1981-07-18 }<br>]"
-var base8 = "[<br>  { nombre: Black Flag, fechaInicio: 1976-01-01 },<br> { nombre: Minor Threat, fechaInicio: 1980-12-14 },<br> { nombre: Bad Brains, fechaInicio: 1977-12-10 },  <br>{ nombre: Agnostic Front, fechaInicio: 1980-11-17 },<br> { nombre: Cro-Mags, fechaInicio: 1981-09-01 },<br> { nombre: Sick of It All, fechaInicio: 1986-03-01 },<br> { nombre: H2O, fechaInicio: 1994-05-01 },<br> { nombre: Madball, fechaInicio: 1988-01-01 },<br> { nombre: Terror, fechaInicio: 2002-01-01 },<br> { nombre: Converge, fechaInicio: 1990-02-17 },<br> { nombre: Refused, fechaInicio: 1991-01-01 },<br> { nombre: Propagandhi, fechaInicio: 1986-12-31 },<br> { nombre: Snapcase, fechaInicio: 1989-01-01 },<br> { nombre: Hatebreed, fechaInicio: 1994-01-01 },<br> { nombre: Comeback Kid, fechaInicio: 2000-01-01}<br>]"
-var base9 = "[<br>  { nombre: Casablanca, precio: 12.99 },<br> { nombre: Gone with the Wind, precio: 15.99 },<br> { nombre: The Godfather, precio: 14.99 }, <br> { nombre: Citizen Kane, precio: 11.99 },<br> { nombre: Singin' in the Rain, precio: 13.99 },<br> { nombre: Psycho, precio: 12.99 }, <br>{ nombre: Lawrence of Arabia, precio: 16.99 },<br> { nombre: The Wizard of Oz, precio: 10.99 }, <br> { nombre: Breakfast at Tiffany's, precio: 13.99 },<br> { nombre: A Streetcar Named Desire, precio: 12.99 },<br> { nombre: Ben-Hur, precio: 15.99 }, <br>{ nombre: Rebel Without a Cause, precio: 11.99 ]<br>"
+var base7 = "[<br>  {nombre: Metallica, fechaInicio: 1981-10-28 },<br> { nombre: Iron Maiden, fechaInicio: 1975-12-25 },<br> { nombre: Black Sabbath, fechaInicio: 1968-02-13 }, <br> { nombre: Slayer, fechaInicio: 1981-01-01 },<br> { nombre: Megadeth, fechaInicio: 1983-06-12 },<br> { nombre: Judas Priest, fechaInicio: 1969-02-01 }<br>]"
+var base8 = "[<br>  { nombre: Black Flag, fechaInicio: 1976-01-01 },<br> { nombre: Minor Threat, fechaInicio: 1980-12-14 },<br> { nombre: Bad Brains, fechaInicio: 1977-12-10 },  <br>{ nombre: Agnostic Front, fechaInicio: 1980-11-17 },<br>] "
+var base9 = "[<br>  { nombre: Casablanca, precio: 12.99 },<br> { nombre: Gone with the Wind, precio: 15.99 },<br> { nombre: The Godfather, precio: 14.99 }, <br> { nombre: Citizen Kane, precio: 11.99 },<br> { nombre: Singin' in the Rain, precio: 13.99 },<br> { nombre: Psycho, precio: 12.99 }, <br>{ nombre: Lawrence of Arabia, precio: 16.99 },<br> { nombre: The Wizard of Oz, precio: 10.99 }<br> ]"
+var base10 = "[<br> { nombre: Pantera, fechaInicio: 1981-08-27 },<br> { nombre: Opeth, fechaInicio: 1989-03-01 },<br> { nombre: Sepultura, fechaInicio: 1984-04-04 },<br> { nombre: Anthrax, fechaInicio: 1981-07-18 }<br>]"
+var base11 = "[<br> { nombre: Converge, fechaInicio: 1990-02-17 },<br> { nombre: Refused, fechaInicio: 1991-01-01 },<br> { nombre: Propagandhi, fechaInicio: 1986-12-31 },<br> { nombre: Snapcase, fechaInicio: 1989-01-01 },<br> { nombre: Hatebreed, fechaInicio: 1994-01-01 },<br> { nombre: Comeback Kid, fechaInicio: 2000-01-01}<br>]"
+var base12 = "[<br> { nombre: Cerveza, precio: 5.99 },<br> { nombre: Vino tinto, precio: 12.75 },<br> { nombre: Champán, precio: 45.50 },<br> { nombre: Brandy, precio: 28.25 },<br> { nombre: Licor de café, precio: 16.80 } <br>]" 
+var base13 = "[<br> { nombre: Breakfast at Tiffany's, precio: 13.99 },<br> { nombre: A Streetcar Named Desire, precio: 12.99 },<br> { nombre: Ben-Hur, precio: 15.99 }, <br>{ nombre: Rebel Without a Cause, precio: 11.99} <br>]"
+var base14 = "[ <br>    { nombre: Juan,    edad: 25,    ciudad: Madrid  }, <br> {    nombre: María,    edad: 30,    ciudad: Barcelona  },<br>  {    nombre: Carlos,    edad: 28,    ciudad: Valencia  }, <br> {    nombre: Ana,    edad: 22,    ciudad: Sevilla  },<br>  {    nombre: Luis,    edad: 35,    ciudad: Bilbao  }<br>]"
+var base15 = "[ <br> { nombre: Cro-Mags, fechaInicio: 1981-09-01 },<br> { nombre: Sick of It All, fechaInicio: 1986-03-01 },<br> { nombre: H2O, fechaInicio: 1994-05-01 },<br> { nombre: Madball, fechaInicio: 1988-01-01 },<br> { nombre: Terror, fechaInicio: 2002-01-01 },<br>]"
+var base16 = "[<br> {    nombre: Leche,    precio: 2.99,    fecha_vencimiento: 2023-07-15  },<br>  {    nombre: Yogur,    precio: 1.25,    fecha_vencimiento: 2023-06-30  },<br>  {    nombre: Pan,    precio: 1.99,    fecha_vencimiento: 2023-06-25  },<br>  {    nombre: Huevos,    precio: 3.49,    fecha_vencimiento: 2023-06-28  }, <br> {    nombre: Queso,    precio: 4.99,    fecha_vencimiento: 2023-07-10  } <br>]"
+var base17  = "[<br>  [1, 2, 3, 4, 5], <br>[rojo, verde, azul], [true, false, true, false], [perro, gato, pájaro, pez], [10.5, 7.8, 3.2, 9.1]<br>]"
+var base18 = "[<br> [Juan, 25, Madrid], <br> [María, 30, Barcelona], <br>[Carlos, 28, Valencia],<br> [Ana, 22, Sevilla], <br>[Luis, 35, Bilbao] <br>]"
+var base19 = "[<br>[Manzana, 0.5, Fruta],<br>[Zanahoria, 0.3, Vegetal],<br> [Pollo, 2.5, Carne],<br>[Arroz, 1.2, Grano],<br>[Leche, 1.0, Lácteo] <br>]"
+var base20 = "[<br> [Toyota, Corolla, 2018], <br>[Honda, Civic, 2019], <br>[Ford, Mustang, 2020], <br>[Chevrolet, Camaro, 2017], <br>[BMW, X5, 2021] <br>]"
 
-
-var basesDedatos =[base1, base2, base3, base4, base5, base6, base7, base8, base9]
+var basesDedatos = [base1, base2, base3, base4, base5, base6, base7, base8, base9, base10, base11, base12, base13, base14, base15, base16, base17, base18, base19, base20]
 var btnBaseDatos = document.getElementById("generar-base-datos")
 var parrafoDatos = document.getElementById("parrafo-base-datos")
 
